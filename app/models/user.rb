8a883eb11f8ef8c8988e41aa630a50668c2dc83f
Henrique Breim <henrique@breim.com.br>
2015-10-16 20:12:45 -0300

    Fix identation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
    # Validations
    validates :name, presence: true
    validate :name_is_not_email
 
-  def name_is_not_email
-    unless (self.name =~ /[@]/) == nil
-      errors.add(:name, "some error")
-    end
-  end
+   def name_is_not_email
+   	unless (self.name =~ /[@]/) == nil
+   		errors.add(:name, "some error")
+   	end
+   end
 
 end
