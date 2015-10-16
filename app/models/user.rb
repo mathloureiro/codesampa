394ff8983995798a3980c67ea696e2d75def64cb
Henrique Breim <henrique@breim.com.br>
2015-10-16 20:10:59 -0300

    Add users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   # Validations
+   validates :name, presence: true
+   validate :name_is_not_email
+
+  def name_is_not_email
+    unless (self.name =~ /[@]/) == nil
+      errors.add(:name, "some error")
+    end
+  end
+
 end
