3c836569cb89ab6631202f42374a0de6a5a8c250
Henrique Breim <henrique@breim.com.br>
2015-10-16 20:39:27 -0300

    Add flash responders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  responders :flash
+
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
