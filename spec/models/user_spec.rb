159b081e255eab828d76842926aa5eea89645e49
Henrique Breim <henrique@breim.com.br>
2015-10-24 20:16:17 -0200

    refactoring user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	it 'Create user test' do
-		create_user = User.create!(name: 'Henrique', email: "#{SecureRandom.hex(5)}@mail.com", password: '12345678abc@')
-		result = User.last.name
-		expect(result).to eq 'Henrique'
+	# Jerk test https://gist.github.com/breim/f3cdeff398015c2ee8d3
+	context "All validatios of model" do
+		# Name is present?
+		it { should validate_presence_of(:name) }
+
+		# User have email?
+		it { should validate_presence_of(:email) }
+
+		# User have password?
+		it { should validate_presence_of(:password) }
+
+		# User password lenght
+		it { should validate_length_of(:password).is_at_least(5) }
 	end
+
 end
