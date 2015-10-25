4fa09dae72ba1ee2e87194b677fea1f780552f17
Henrique Breim <henrique@breim.com.br>
2015-10-25 04:40:33 -0200

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,20 @@ class ApplicationController < ActionController::Base
   # Devise options
   helper_method :current_user
   
-private
-   before_filter :configure_devise_params, if: :devise_controller?
+  private
+  before_filter :configure_devise_params, if: :devise_controller?
   def configure_devise_params
   	devise_parameter_sanitizer.for(:sign_up) do |u|
-  		u.permit(:name,:email,:password,:password_confirmation)
+  		u.permit(:name, :email, :password, :password_confirmation)
   	end
   	devise_parameter_sanitizer.for(:account_update) do |u|
-  		u.permit(:name,:email,:password,:password_confirmation)
+  		u.permit(:name, :email, :password, :password_confirmation)
   	end
   end
 
-  def after_sign_in_path_for(resource_or_scope)
-    session[:previous_url] || courses_path
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
-  
+
 end
