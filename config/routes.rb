3950b1dc7615025df0339f1bac66fc61b4ac0bf6
Henrique Breim <henrique@breim.com.br>
2015-10-26 16:23:27 -0200

    Add concepts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
 	root 'pages#index'
 	devise_for :users
   	resources :pages, only: [:index]
-  	resources :courses, only: [:index, :show], path: 'cursos'
+  	resources :courses, only: [:index, :show], path: 'cursos' do
+      resources :concepts, only: [:show], path: 'conceitos'
+    end
+
 
   	get '/admin' => "admin/admin#index", as: :admin
   	namespace :admin do
   		resources :courses
+      resources :concepts
   	end
 end
