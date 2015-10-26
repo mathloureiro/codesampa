727b23214d227df49ff61e228551db08b3cac13d
Henrique Breim <henrique@breim.com.br>
2015-10-26 16:24:16 -0200

    Reindentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-
 	root 'pages#index'
-	devise_for :users
-  	resources :pages, only: [:index]
-  	resources :courses, only: [:index, :show], path: 'cursos' do
-      resources :concepts, only: [:show], path: 'conceitos'
-    end
+	
+  devise_for :users
+  
+  resources :pages, only: [:index]
+  
+  resources :courses, only: [:index, :show], path: 'cursos' do
+    resources :concepts, only: [:show], path: 'conceitos'
+  end
 
+  get '/admin' => "admin/admin#index", as: :admin
+  namespace :admin do
+    resources :courses
+    resources :concepts
+  end
 
-  	get '/admin' => "admin/admin#index", as: :admin
-  	namespace :admin do
-  		resources :courses
-      resources :concepts
-  	end
 end
